52cefcce2c7dd9b2310e4a850a63607cf3bdd554
mgkkawa <k.kawate@mg-k.co.jp>
2022-04-26 17:38:32 +0900

    1.1.1

--- src/_main_test.ts
+++ src/_main_test.ts
@@ -1,73 +1,3 @@
-
-const addShiftObject = (date = new Date()) => {
-  const shift = shiftObjectCheck(date);
-
-  const nh = mainData_('nh');
-  const nh_sheet = nh.getSheetByName(valueDate(date, 'yyyy.MM'));
-  const nh_label = nh_sheet.getRange(1, 1, 1, nh_sheet.getLastColumn()).getValues().flat();
-  const name_ind = nh_label.indexOf('氏名');
-  const nh_data = nh_sheet.getDataRange().getValues().filter(values => values[name_ind] != '氏名' && values[name_ind] != '')
-    .map(values => ['氏名', 'MTG', '病欠', '当欠', '研修'].map(key => values[nh_label.indexOf(key)]));
-
-  nh_data.forEach(values => {
-    const staff = values[0];
-    const mtg = values[1].replace(/日/g, '').padStart(2, '0');
-    const sick = JSON.parse(`[${values[2].replace(/日/g, '')}]`).filter(Boolean);
-    const absent = JSON.parse(`[${values[3].replace(/日/g, '')}]`).filter(Boolean);
-    const training = JSON.parse(`[${values[4].replace(/日/g, '')}]`).filter(Boolean);
-
-    if (mtg.length != 0) {
-      const check = shift[mtg][staff]['FLAG']
-      if (check) {
-        shift[mtg][staff]['FLAG'] = false;
-        shift[mtg][staff]['SET'] = 'M';
-      } else {
-        try { Browser.msgBox(`MTGチェック${staff}さんの${mtg}日シフトは\n"${shift[mtg][staff]['SET']}"です`) }
-        catch { Logger.log(`MTGチェック${staff}さんの${mtg}日シフトは\n"${shift[mtg][staff]['SET']}"です`) };
-      }
-    }
-    if (sick.length > 0) {
-      sick.forEach(d => {
-        const dd = String(d).padStart(2, '0');
-        const check = shift[dd][staff]['FLAG'];
-        if (check) {
-          shift[dd][staff]['FLAG'] = false;
-          shift[dd][staff]['SET'] = '病欠';
-        } else {
-          try { Browser.msgBox(`病欠チェック${staff}さんの${dd}日シフトは\n"${shift[dd][staff]['SET']}"です`) }
-          catch { Logger.log(`病欠チェック${staff}さんの${dd}日シフトは\n"${shift[dd][staff]['SET']}"です`) };
-        }
-      });
-    };
-    if (absent.length > 0) {
-      absent.forEach(d => {
-        const dd = String(d).padStart(2, '0');
-        const check = shift[dd][staff]['FLAG'];
-        if (check) {
-          shift[dd][staff]['FLAG'] = false;
-          shift[dd][staff]['SET'] = '当欠';
-        } else {
-          try { Browser.msgBox(`当欠チェック${staff}さんの${dd}日シフトは\n"${shift[dd][staff]['SET']}"です`) }
-          catch { Logger.log(`当欠チェック${staff}さんの${dd}日シフトは\n"${shift[dd][staff]['SET']}"です`) };
-        }
-      });
-    };
-    if (training.length > 0) {
-      training.forEach(d => {
-        const dd = String(d).padStart(2, '0');
-        const check = shift[dd][staff]['FLAG'];
-        if (check) {
-          shift[dd][staff]['SET'] = '研';
-        } else {
-          try { Browser.msgBox(`研修チェック${staff}さんの${dd}日シフトは\n"${shift[dd][staff]['SET']}"です`) }
-          catch { Logger.log(`研修チェック${staff}さんの${dd}日シフトは\n"${shift[dd][staff]['SET']}"です`) };
-        }
-      });
-    };
-  });
-  shiftObjectUpdate(shift, date);
-}
-
 //アサインシートをオブジェクト化（途中）
 const assign_object = () => {
   const yyyy = '2021';
@@ -135,15 +65,15 @@ const assign_object = () => {
       obj['hold'] = trim[i][keys.indexOf('開催\n可否')];
       obj['course'] = trim[i][keys.indexOf('コース')];
       if (flag == 'エムジー') {
-        obj['meeting'] = shiftMainStart(start);
+        obj['meeting'] = timeStartMain(start);
         mem_obj['main'] = trim[i][keys].indexOf('メイン\n講師');
       } else {
-        obj['meeting'] = shiftSupStart(start);
+        obj['meeting'] = timeStartSup(start);
         mem_obj['main'] = null;
       }
       obj['start'] = valueDate(start, 'H:mm');
       obj['finish'] = valueDate(finish, 'H:mm');
-      obj['leave'] = shiftEnd(finish);
+      obj['leave'] = timeEnd(finish);
       obj['limit'] = trim[i][keys.indexOf('定員\n(半角)')];
       obj['plan'] = trim[i][keys.indexOf('実参加\n人数')];
       obj['update'] = trim[i][keys.indexOf('更新日')];
@@ -198,104 +128,8 @@ const shiftCheck = (date = new Date()) => {
   }
   wr.getSheetByName('シート2').getRange(3, 2, array.length, array[0].length)
     .setValues(array);
-}
-const shiftinfo = (date = new Date()) => {
-  const yyyy = valueDate(date, 'yyyy');
-  const MM = valueDate(date, 'MM');
-  const shift = shiftObjectCheck();
-  const staff_obj = staffObject_();
-  const staffs = Object.keys(staff_obj);
+};
 
-  const as = mainData_('as');
-  const as_sheet = as.getSheetByName(valueDate(date, 'yyyyMM'));
-  const as_data = as_sheet.getDataRange().getValues();
-  let ind = 0;
-  const as_label = as_data.filter((values, index) => {
-    if (values.includes('日程')) { ind = index + 2; };
-    return values.includes('日程');
-  }).flat();
-  const as_days = as_data.flatMap(values => values[as_label.indexOf('日程')])
-    .map(value => valueDate(value, 'dd'));
-  const trim_days = as_days.filter((value, index, array) => String(value).match(/[\d]/) && array.indexOf(value) == index);
-
-  trim_days.forEach(dd => {
-    staffs.forEach(staff => {
-      let count = 1;
-      const work = shift[MM][dd][staff];
-      if (work['judge'] == '出勤') {
-        const info = {};
-        for (let i = as_days.indexOf(dd); i <= as_days.lastIndexOf(dd); i++) {
-          if (as_data[i].includes(staff)) {
-            work['venue'] = as_data[i][as_label.indexOf('会場\n名称')];
-            const time = {};
-            const start = new Date(as_data[i][as_label.indexOf('開始')]);
-            const end = new Date(as_data[i][as_label.indexOf('終了')]);
-            const flag_check = (as_data[i][as_label.indexOf('講師')] == 'エムジー');
-            if (flag_check) {
-              const main_check = (as_data[i][as_label.indexOf('メイン\n講師')] == staff);
-              if (main_check) {
-                work['flag'] = 'メイン';
-              } else {
-                work['flag'] = 'サポート';
-              }
-              time['meeting'] = shiftMainStart(start);
-
-            } else {
-              work['flag'] = 'SB同行';
-              time['meeting'] = shiftSupStart(start);
-            }
-            time['start'] = valueDate(start, 'H:mm');
-            time['end'] = valueDate(end, 'H:mm');
-            time['leave'] = shiftEnd(end);
-            info[String(count).padStart(2, '0')] = time;
-            work['info'] = info;
-            ++count;
-          }
-        }
-      }
-    })
-  });
-
-  const database = mainData_('db').getSheetByName('DB');
-
-  let set_obj = JSON.stringify(shift);
-  const set_strings = new Array(Math.ceil(set_obj.length / 50000));
-  if (set_strings.length > 1) {
-    for (let l = 0; l < set_strings.length; l++) {
-      set_strings.splice(l, 1, set_obj.slice(l * 50000, (l + 1) * 50000));
-    }
-  } else {
-    set_strings[set_strings.length - 1] = set_obj;
-  }
-  database.getRange(database.getLastRow() + 1, 1, 1, set_strings.length + 2)
-    .setValues([[yyyy, MM].concat(set_strings)]);
-  return;
-}
-
-const obj_set = (obj) => {
-  const date = new Date();
-  const yyyy = valueDate(date, 'yyyy');
-  const MM = valueDate(date, 'MM');
-  const database = mainData_('db').getSheetByName('DB');
-  let set_obj = JSON.stringify(obj);
-  const set_strings = new Array(Math.ceil(set_obj.length / 50000));
-
-  if (set_strings.length > 1) {
-    for (let l = 0; l < set_strings.length; l++) {
-      set_strings.splice(l, 1, set_obj.slice(l * 50000, (l + 1) * 50000));
-    }
-  } else {
-    set_strings[set_strings.length - 1] = set_obj;
-  }
-  database.getRange(database.getLastRow() + 1, 1, 1, set_strings.length + 2)
-    .setValues([[yyyy, MM].concat(set_strings)]);
-}
-const castingTest = () => {
-  const vc = testData_('vc');
-  const sh = testData_('sh');
-
-
-}
 const testEcho = () => {
   console.log('consoleテスト成功!!');
   Logger.log('Loggerテスト成功!!');
@@ -322,6 +156,7 @@ const judge = (value) => {
     case 'リ': return 'リフレ';
     case 'M': return 'ASB';
     case '研': return '研修';
+    case '忌引': return '忌引';
     default: return '出勤';
   }
 }
